455ec57410c9c1627ffcbe5dda5b5c67ab5dc7b7
Lucas Früchtl <lucas.fruechtl@company4.de>
2023-06-02 11:14:10 +0200

    add zsh installer

---
+++ ohmyzsh.sh
@@ -0,0 +1,2 @@
+# Installs ZSH, ohmyzsh and zsh-autosuggestions. add zsh-autosuggestions to plugins in .zshrc
+sudo apt install zsh && chsh -s $(which zsh) && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions && sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
